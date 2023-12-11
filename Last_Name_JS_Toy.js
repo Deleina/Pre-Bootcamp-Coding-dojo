2c25529e413ebb6fba38c044d62e911b4d31844a
Deleina Llamocca <marta.deleina@gmail.com>
2023-12-11 18:35:21 -0500

    Optional JavaScript Code Challenge

---
+++ Last_Name_JS_Toy.js
@@ -0,0 +1,9 @@
+const childHeight = 44;
+
+function ifTheChildCanGoUpTheRollerCoaster(){
+    if (childHeight > 52) {
+        console.log('subete niño!:D');
+    }else{
+        console.log('Lo siento, chico. Tal vez el próximo año :/');
+    }
+}
